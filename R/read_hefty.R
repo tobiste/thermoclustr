174721ea4c43e8056a2bb8dc860573f8c7a0808f
Tobias Stephan <73840881+tobiste@users.noreply.github.com>
2025-06-25 17:48:10 -0400

    faster import

--- R/read_hefty.R
+++ R/read_hefty.R
@@ -51,14 +51,20 @@ assign_segment <- function(x) { # create new function called assign_segment
 #' @returns list of character vectors, where each vector corresponds to a line in the file.
 #' @noRd
 parse_hefty <- function(fname){
-  f <- data.table::fread(fname, sep = "\n", header = FALSE, skip = 0, colClasses = 'character', blank.lines.skip	= TRUE, col.names = "") |>
-    lapply(function(x) {
-      strsplit(x, "\t")
-    })
-  f[[1]]
+  # f <- data.table::fread(fname, sep = "\n", header = FALSE, skip = 0, colClasses = 'character', blank.lines.skip	= TRUE, col.names = "", , encoding = "UTF-8") |>
+  #   lapply(function(x) {
+  #     strsplit(x, "\t")
+  #   })
+  # f[[1]]
+  lines = fread( fname, sep = "\n", header = FALSE, colClasses = "character", encoding = "UTF-8")[[1]]
+  lines <-   iconv(lines, from = "", to = "UTF-8", sub = "byte")
+  strsplit(lines, "\t", useBytes = TRUE)
+  
 }
 
-
+list_to_matrix <- function(x, ncol = NULL, byrow = TRUE) {
+  matrix(unlist(x, use.names = FALSE), ncol = ncol %||% length(x[[1]]), byrow = byrow)
+}
 
 #' Read time, temperature and GOF data from HeFTy output from excel file
 #'
@@ -120,38 +126,13 @@ read_hefty_xlsx <- function(fname) {
 #' read_hefty(path2myfile)
 read_hefty <- function(fname) {
   Fit <- value <- Comp_GOF <- time <- segment <- temperature <- V1 <- V2 <- V3 <- V4 <- V5 <- constraint <- NULL
-  # file <- readLines(fname) |>
-  #   strsplit("\t", useBytes = TRUE)
-  
   file <- parse_hefty(fname)
   
   # extract individual paths
-  individual_paths_loc <- grep("Individual paths", file)
-  # individual_paths_mat <- do.call(rbind, file[(individual_paths_loc + 4):length(file)])
-  
-  # repeated rbind is slow:
+  individual_paths_loc <- match("Individual paths", sapply(file, `[`, 1))
   individual_data <- file[(individual_paths_loc + 4):length(file)]
-  individual_paths_mat <- matrix(
-    unlist(individual_data, use.names = FALSE),
-    ncol = length(individual_data[[1]]),
-    byrow = TRUE
-  )
+  individual_paths_mat <- list_to_matrix(individual_data)
   
-  # odd <- seq_along(individual_paths_mat[, 1]) %% 2 == 0
-  # GOF <- individual_paths_mat[odd, 2] |>
-  #   as_tibble() |>
-  #   rename(Comp_GOF = value) |>
-  #   mutate(
-  #     Comp_GOF = as.numeric(Comp_GOF),
-  #     segment = as.factor(dplyr::row_number()),
-  #     Fit = dplyr::case_when(
-  #       Comp_GOF >= 0.9 ~ "Best",
-  #       dplyr::between(Comp_GOF, 0.5, 0.9) ~ "Good",
-  #       dplyr::between(Comp_GOF, 0.05, .5) ~ "Acceptable",
-  #       .default = NA
-  #     ),
-  #     Fit = factor(Fit, levels = c(NA, "Acceptable", "Good", "Best"))
-  #   )
   odd_idx <- seq(2, nrow(individual_paths_mat), by = 2)
   Comp_GOF <- as.numeric(individual_paths_mat[odd_idx, 2])
   GOF <- data.frame(
@@ -164,21 +145,9 @@ read_hefty <- function(fname) {
       TRUE ~ NA_character_
     )
   )|>
-    dplyr::mutate(Fit = factor(Fit, levels = c(NA, "Acceptable", "Good", "Best"))) #|> 
-  # dplyr::arrange(Fit, Comp_GOF) 
-  
+    dplyr::mutate(Fit = factor(Fit, levels = c(NA, "Acceptable", "Good", "Best")))
   time_loc <- grep("Time", individual_paths_mat[1, ]) + 1
-  # paths <- individual_paths_mat[, time_loc:ncol(individual_paths_mat)] |>
-  #   as_tibble() |>
-  #   combine_rows() |>
-  #   # dplyr::filter(!is.na(time) & !is.na(temperature)) |>
-  #   mutate(segment = assign_segment(time)) |>
-  #   dplyr::right_join(GOF, by = "segment") |>
-  #   dplyr::select(segment, time, temperature, Fit, Comp_GOF) |>
-  #   dplyr::arrange(Fit, Comp_GOF) |>
-  #   dplyr::mutate(segment = forcats::fct_reorder(segment, Comp_GOF)) |>
-  #   as_tibble()
-  #faster:
+  
   path_data <- individual_paths_mat[, time_loc:ncol(individual_paths_mat)] 
   paths <- combine_rows(path_data)|>
     dplyr::mutate(
@@ -190,34 +159,21 @@ read_hefty <- function(fname) {
     dplyr::mutate(segment = forcats::fct_reorder(segment, Comp_GOF))
   
   # extract weighted mean path
-  wm_path_loc <- grep("Weighted mean path", file)
-  # wm <- do.call(rbind, file[wm_path_loc + c(1, 2)]) |>
-  #   t() |>
-  #   utils::tail(-1) |>
-  #   as_tibble() |>
-  #   rename(time = V1, temperature = V2) |>
-  #   mutate(across(everything(), as.numeric))
+  wm_path_loc <- match("Weighted mean path", sapply(file, `[`, 1))
   wm_data <- file[wm_path_loc + c(1, 2)]
-  wm0 <- matrix(unlist(wm_data, use.names = FALSE), ncol = 2, byrow = F)
+  wm0 <- list_to_matrix(wm_data, ncol = 2, byrow = FALSE)
   wm_length <- nrow(wm0)
+  wm_numeric <- utils::tail(wm0, -1)
   wm <- data.frame(
-    time = as.numeric(wm0[2:wm_length, 1]),
-    temperature = as.numeric(wm0[2:wm_length, 2])
+    time = as.numeric(wm_numeric[, 1]),
+    temperature = as.numeric(wm_numeric[, 2])
   )
   
   # extract constraints
   inversion_terminated_loc <- grep("Inversion", file)
-  # constr1 <- do.call(rbind, file[3:inversion_terminated_loc - 1])
-  # constraints <- constr1[, 1:5] |>
-  #   utils::tail(-1) |>
-  #   as_tibble() |>
-  #   rename("constraint" = V1, "max_time" = V2, "min_time" = V3, "max_temp" = V4, "min_temp" = V5) |>
-  #   mutate(across(everything(), as.numeric),
-  #     constraint = forcats::as_factor(constraint)
-  #   )
-  # Faster:
   constraint_data <- file[3:(inversion_terminated_loc - 1)]
   constraint_mat <- matrix(unlist(constraint_data, use.names = FALSE), nrow = 5, byrow = TRUE)[, 1:5]
+
   constraints <- data.frame(
     constraint = as.factor(constraint_mat[, 1]),
     max_time = as.numeric(constraint_mat[, 2]),
@@ -227,15 +183,9 @@ read_hefty <- function(fname) {
   )
   
   summaries_loc <- grep("Summaries", file)
-  # grain_summary <- do.call(rbind, file[summaries_loc:(wm_path_loc - 1)]) |>
-  #   t() |>
-  #   utils::tail(-1) |>
-  #   as_tibble() |>
-  #   rename("grain" = V1, "mean" = V2, sd = V3, min = V4, max = V5) |>
-  #   mutate(across(!matches("grain"), as.numeric))
-  #   FASTER:
   summary_data <- file[summaries_loc:(wm_path_loc - 1)]
-  summary_mat <- matrix(unlist(summary_data, use.names = FALSE), ncol = 5, byrow = FALSE)[-1, ]
+  summary_mat <- list_to_matrix(summary_data, ncol = 5, byrow = FALSE)[-1, ]
+  
   grain_summary <- data.frame(
     grain = summary_mat[, 1],
     mean = as.numeric(summary_mat[, 2]),
