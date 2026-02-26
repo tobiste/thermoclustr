339d07262ecc47eec18c0bbfda1abb30a7d0a1d5
Tobias Stephan <73840881+tobiste@users.noreply.github.com>
2026-02-26 16:25:46 -0500

    bug fix

--- R/thermoclustr.R
+++ R/thermoclustr.R
@@ -1,8 +1,8 @@
 #' @keywords internal
 ## usethis namespace: start
 #' @importFrom Rcpp sourceCpp
-#' @useDynLib thermochron, .registration = TRUE
+#' @useDynLib thermoclustr, .registration = TRUE
 ## #' @importFrom Rcpp sourceCpp
-## #' @useDynLib thermochron, .registration = TRUE
+## #' @useDynLib thermoclustr, .registration = TRUE
 ## usethis namespace: end
 "_PACKAGE"
